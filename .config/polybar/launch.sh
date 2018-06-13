ee225694d503e56ed90d65dfd14c64f8ca692496
Cat Flynn <mewtwo97@hotmail.com>
2018-06-13 19:51:40 +0100

    Clear up defunct polybar stuff

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# gaps mode set by bindsyms in i3 conf
-modepath='/home/cat/.config/i3/mode.txt'
-
 # Terminate already running bar instances
 killall -q polybar
-
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep1; done
 
